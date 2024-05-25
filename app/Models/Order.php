c1ad0b4afd00afb704b842b4f88d007a5b06f015
Mahdi Amereh <mahdieightt@gmail.com>
2024-05-26 02:47:07 +0330

    feat: add property fillable to order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,19 @@ class Order extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'source_address',
+        'source_lat',
+        'source_long',
+        'destination_address',
+        'destination_lat',
+        'destination_long',
+        'sender_name',
+        'sender_mobile',
+        'receiver_name',
+        'receiver_mobile',
+        ];
+
     public function trips()
     {
         return $this->hasMany(Trip::class);
